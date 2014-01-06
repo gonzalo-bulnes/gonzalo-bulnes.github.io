28c161d9d3f943335c450c4ea6c23fbbfb45379e
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-01-06 00:50:27 -0300

    Add jQuery Date Format
    See https://github.com/phstc/jquery-dateFormat

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -1,3 +1,5 @@
+//= require 'vendor/jquery-1.9.1.min'
+//= require 'vendor/jquery-dateFormat-8cb2b5d5067cb4.min'
 //= require 'vendor/github_pages/scale.fix'
 
 var greeting = function() { return 'Hello!' }; // trigger the Jekyll Assets pipeline
